950a4c97bd3cc1332d1e366ec8029c0a8aad4b56
Equipo4DevSecOps <maynor_hernandez01@unitec.edu>
2025-12-16 19:39:06 -0600

    Commit

--- security/sast.sh
+++ security/sast.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Running SAST..."
 
-APP_DIR="./todo-app"  # Ajusta al path real de tu app en Jenkins
+APP_DIR="./todo-app"
 
-# Buscar eval( y exec( solo dentro de la app
+# Buscar eval( y exec( solo dentro de la app, mostrar advertencia sin fallar
 if grep -R --exclude=security/sast.sh --exclude-dir=.git "eval(" "$APP_DIR"; then
     echo "⚠ Warning: 'eval(' detected in code"
 fi
